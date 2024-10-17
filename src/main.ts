cf3c38a7459d0709c0a2fae78bd3485c148d755f
Vinoth Ari <148189396+vinoth-ari-ey@users.noreply.github.com>
2024-10-17 17:05:59 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import { animate, createBall } from "./helpers";
 import { State } from "./types";
 
 // -- Constants values
-export const siteCode = "0byr9yahtt";
-export const featureKey = "bb_demo";
+export const siteCode = "na3r8z406r";
+export const featureKey = "bouncing_ball_demo";
 
 // -- Define the default state
 const state: State = {
@@ -19,7 +19,11 @@ const state: State = {
 };
 
 // -- Configure the SDK
-const client = new KameleoonClient({ siteCode });
+// const client = new KameleoonClient({ siteCode });
+const configuration = {
+   updateInterval: 1,
+};
+const client = new KameleoonClient({ siteCode, configuration });
 
 async function init(): Promise<void> {
   // -- Initialize the SDK
